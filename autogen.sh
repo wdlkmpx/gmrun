1c193a3fc4925df0a19164d980c3c3da9be4f98d
wdlkmpx <wdlkmpx@gmail.com>
2021-04-07 10:32:19 +0800

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,40 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+# special params:
+# - po       update .pot & po files
+# - linguas  update LINGUAS and POTFILES.in
+# - release  create release tarball
+
+#===========================================================================
+
+if test "$1" =  "po" ; then
+	# Before creating a release you might want to update the po files
+	test -d "po/"         || exit
+	test -f ./configure   || ./autogen.sh
+	test -f ./po/Makefile || ./configure
+	make -C po update-po
+	# cleanup
+	rm -f po/*.po~
+	sed -i '/#~ /d' po/*.po
+	#git clean -dfx
+	exit
+fi
+
+if test "$1" =  "linguas" ; then
+	test -d "po/" || exit
+	pot_in=$(grep '_(' $(find . -type f -name '*.h' -or -name '*.c') | sed -e 's%^\./%%' -e 's%:.*%%' | sort -u)
+	echo "${pot_in}" > po/POTFILES.in
+	linguas=$(find po -name '*.po' | sed -e 's%.*/%%' -e 's%\.po%%' | sort)
+	echo ${linguas} > po/LINGUAS
+	exit
+fi
+
+#===========================================================================
+
 if test "$1" == "release" || test "$1" == "--release" ; then
-	pkg="$(grep -m1 AC_INIT configure.ac | cut -f 2 -d '[' | cut -f 1 -d ']')"
-	ver="$(grep -m1 AC_INIT configure.ac | cut -f 3 -d '[' | cut -f 1 -d ']')"
+	pkg="$(grep -m 1 AC_INIT configure.ac | cut -f 2 -d '[' | cut -f 1 -d ']')"
+	ver="$(grep -m 1 AC_INIT configure.ac | cut -f 3 -d '[' | cut -f 1 -d ']')"
 	ver=$(echo $ver)
 	dir=${pkg}-${ver}
 	rm -rf ../$dir
@@ -15,6 +46,9 @@ if test "$1" == "release" || test "$1" == "--release" ; then
 	exit
 fi
 
+
+#===========================================================================
+#                    autogen.sh [--verbose]
 #===========================================================================
 
 srcdir=`dirname $0`
@@ -36,16 +70,20 @@ fi
 
 # Get all required m4 macros required for configure
 $LIBTOOLIZE ${verbose} --copy --force || exit 1
-$ACLOCAL ${verbose} -I m4 || exit 1
+$ACLOCAL ${verbose} || exit 1
 
 # Generate config.h.in
 $AUTOHEADER ${verbose} --force || exit 1
 
 # Generate Makefile.in's
-touch config.rpath
+auxdir='.'
+if grep "AC_CONFIG_AUX_DIR" configure.ac ; then
+	auxdir="$(grep AC_CONFIG_AUX_DIR configure.ac | cut -f 2 -d '[' | cut -f 1 -d ']')"
+fi
+touch ${auxdir}/config.rpath
 $AUTOMAKE ${verbose} --add-missing --copy --force || exit 1
 
-if grep "IT_PROG_INTLTOOL" configure.ac >/dev/null ; then
+if grep -q "IT_PROG_INTLTOOL" configure.ac ; then
 	intltoolize ${verbose2} -c --automake --force || exit 1
 	# po/Makefile.in.in has these lines:
 	#    mostlyclean:
